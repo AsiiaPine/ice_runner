3bdbf4ffaface339ca12d69f5c32cb825110e367
AsiiaPine <asiiapine@gmail.com>
2024-10-28 17:44:57 +0300

    worked on server

--- src/bot/mosquitto_sub.py
+++ src/bot/mosquitto_sub.py
@@ -10,13 +10,13 @@ def on_message(client, userdata, msg):
 client = mqtt_client.Client(client_id="", clean_session=True, userdata=None, protocol=MQTTv311)
 client.on_message = on_message
 
-if client.connect("localhost", 1883, 60) != 0:
-# if client.connect("10.100.8.235", 1883, 60) != 0:
+# if client.connect("localhost", 1883, 60) != 0:
+if client.connect("10.100.8.235", 1883, 60) != 0:
     print("Couldn't connect to MQTT broker!")
     sys.exit(-1)
 
 client.subscribe("test/status")
-
+client.subscribe("uavcan.protocol.NodeStatus")
 try:
     print("Press CTRL+C to exit")
     client.loop_forever()
