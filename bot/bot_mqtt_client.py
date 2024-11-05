7a80fea8920cb027621b45ee7604cf533be24d41
AsiiaPine <asiiapine@gmail.com>
2024-11-05 17:04:35 +0300

    bot works

--- bot/bot_mqtt_client.py
+++ bot/bot_mqtt_client.py
@@ -1,50 +1,38 @@
-import asyncio
-import yaml
-import ast
-# from dataclasses import dataclass
-from enum import IntEnum
 from typing import Any, Dict
-
 from paho import mqtt
 from paho.mqtt.client import MQTTv311, Client
-from asyncio_paho import AsyncioPahoClient
 
-async def dummy():
-    pass
 
 class BotMqttClient:
-    client = AsyncioPahoClient(client_id="bot", clean_session=True, userdata=None, protocol=MQTTv311, reconnect_on_failure=True, loop=dummy)
-    # client = Client(client_id="bot", clean_session=True, userdata=None, protocol=MQTTv311, reconnect_on_failure=True)
+    # client: AsyncioPahoClient = None
+    client = Client(client_id="bot", clean_session=True, userdata=None, protocol=MQTTv311, reconnect_on_failure=True)
     rp_states: Dict[int, str] = {}
     rp_status: Dict[int, str] = {}
 
     @classmethod
     async def connect(cls, server_ip: str = "localhost", port: int = 1883, max_messages: int = 10) -> None:
-        async with AsyncioPahoClient(client_id="bot", clean_session=True, userdata=None, protocol=MQTTv311, reconnect_on_failure=True) as client:
-            cls.client = client
-            # cls.client = AsyncioPahoClient()
-            await cls.client.asyncio_connect(server_ip, port, 60)
-            await cls.client.asyncio_publish("ice_runner/bot", "ready")
-            print("BotMqttClient connected")
+        cls.client = Client(client_id="bot", clean_session=True, userdata=None, protocol=MQTTv311, reconnect_on_failure=True)
+        cls.client.connect(server_ip, port, 60)
+        cls.client.publish("ice_runner/bot", "ready")
+        print("BotMqttClient connected")
 
     @classmethod
     def get_client(cls) -> mqtt.client.Client:
         return cls.client
 
-# @BotMqttClient.client.topic_callback(f"ice_runner/server/bot_commander/rp_states/+/state")
-async def handle_commander_state(client, userdata, message):
+def handle_commander_state(client, userdata, message):
     print(f"Bot received message user:{userdata} {message.topic}: {message.payload.decode()}")
     rp_pi_id = int(message.topic.split("/")[-2])
     BotMqttClient.rp_states[rp_pi_id] = message.payload.decode()
 
-# @BotMqttClient.client.topic_callback(f"ice_runner/server/bot_commander/rp_states/+/stats")
-async def handle_commander_stats(client, userdata, message):
+def handle_commander_stats(client, userdata, message):
     print(f"Bot received message user:{userdata} {message.topic}: {message.payload.decode()}")
     rp_pi_id = int(message.topic.split("/")[-2])
     BotMqttClient.rp_status[rp_pi_id] = message.payload.decode()
 
 async def start() -> None:
-    BotMqttClient.client.asyncio_listeners.message_callback_add("ice_runner/bot_commander/rp_states/+/state", handle_commander_state)
-    BotMqttClient.client.asyncio_listeners.message_callback_add("ice_runner/bot_commander/rp_states/+/stats", handle_commander_stats)
-    await BotMqttClient.client.asyncio_subscribe("ice_runner/server/bot_commander/#")
-    # BotMqttClient.client.asyncio_listeners.("ice_runner/server/bot_commander/#")
+    print("start")
+    BotMqttClient.client.message_callback_add("ice_runner/server/bot_commander/rp_states/+/state", handle_commander_state)
+    BotMqttClient.client.message_callback_add("ice_runner/server/bot_commander/rp_states/+/stats", handle_commander_stats)
+    BotMqttClient.client.subscribe("ice_runner/server/bot_commander/#")
+    BotMqttClient.client.loop_start()
--- bot/main.py
+++ bot/main.py
@@ -25,28 +25,20 @@ TOKEN = os.getenv("BOT_TOKEN")
 async def start_bot() -> None:
     config_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'last_config.yml'))
     handlers.set_configuration(config_file_path)
-    # handlers.setup_mqtt_client(BotMqttClient)
     load_dotenv()
     TOKEN = os.getenv("BOT_TOKEN")
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     print("TG:\t" + config_file_path)
     await bot.set_chat_menu_button(menu_button=MenuButtonCommands())
     await BotMqttClient.connect()
-    async def main() -> None:
-        await asyncio.gather(handlers.dp.start_polling(bot), start())
-    asyncio.run(main())
-
-
-    # start()
-    # # BotMqttClient.client.loop_forever()
-    # while True:
-    #     BotMqttClient.client.publish("ice_runner/bot", "keep alive")
-    #     time.sleep(1)
-    #     pass
-
-# async def start_bot() -> None:
+    # await handlers.dp.start_polling(bot)
+    # await start()
+    # Run both the Telegram bot and MQTT listener in parallel
+    await asyncio.gather(
+        handlers.dp.start_polling(bot),
+        start()  # Assuming `start` is your MQTT listener coroutine
+    )
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(start_bot())
-    # asyncio.run(main())
