40b325cccf7f9c25cc02f6c044fc6fd1c9b2c06c
Anastasiia Stepanova <asiiapine@gmail.com>
2024-12-12 17:03:47 +0300

    upd spin

--- raspberry/ice_commander.py
+++ raspberry/ice_commander.py
@@ -127,7 +127,7 @@ class DronecanCommander:
 
     @classmethod
     def spin(cls) -> None:
-        cls.node.node.spin(0.01)
+        cls.node.node.spin(0.05)
         # self.node
         if time.time() - cls.prev_broadcast_time > 0.1:
             cls.prev_broadcast_time = time.time()
