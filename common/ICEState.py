245ef4742c3afffe0c5fbe3e6432f629fdceac04
Anastasiia Stepanova <asiiapine@gmail.com>
2024-12-20 10:11:47 +0300

    add more info about connection, add debug print

--- common/ICEState.py
+++ common/ICEState.py
@@ -42,6 +42,7 @@ def get_recip_state_name(state) -> str:
     return "UNKNOWN"
 
 def get_health_name(health: int) -> str:
+    print(health)
     if health == HealthDict["HEALTH_OK"]:
         return "HEALTH_OK"
     if health == HealthDict["HEALTH_WARNING"]:
