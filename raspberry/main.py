29e632f2a5fc9bef60e5cba5a23e64430ae22159
AsiiaPine <asiiapine@gmail.com>
2025-02-10 21:25:13 +0300

    fix shebang

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,9 +1,10 @@
-"""The script is used to start the engine running-in."""
-
 #!/usr/bin/env python3
 # This software is distributed under the terms of the MIT License.
 # Copyright (c) 2024 Anastasiia Stepanova.
 # Author: Anastasiia Stepanova <asiiapine@gmail.com>
+
+"""The script is used to start the engine running-in."""
+
 
 import os
 import sys
