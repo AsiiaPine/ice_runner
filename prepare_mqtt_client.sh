57652dfd02cf655c81454da9b12daedebf73d135
Anastasiia Stepanova <asiiapine@gmail.com>
2025-02-10 14:39:34 +0300

    made main executable

--- prepare_mqtt_client.sh
+++ prepare_mqtt_client.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # This software is distributed under the terms of the MIT License.
 # Copyright (c) 2024 Anastasiia Stepanova.
 # Author: Anastasiia Stepanova <asiiapine@gmail.com>
--- prepare_mqtt_server.sh
+++ prepare_mqtt_server.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # used pipeline from https://santoshk.dev/posts/2023/mqtt-basics-and-how-to-get-started-on-ubuntu/
 apt-get update
 apt-get install curl gnupg2 wget git apt-transport-https ca-certificates -y
